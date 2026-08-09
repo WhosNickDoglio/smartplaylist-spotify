49b10144368f822a01d25686af66de58ccfc2dd2
nicholasdoglio <NicholasDoglio@Gmail.com>
2026-08-08 22:25:59 -0400

    Provide ApplicationScope

---
+++ libraries/concurrency/public/src/commonMain/kotlin/dev/whosnickdoglio/spot/concurrency/ApplicationScope.kt
@@ -0,0 +1,8 @@
+// Copyright (C) 2026 Nicholas Doglio
+// SPDX-License-Identifier: MIT
+
+package dev.whosnickdoglio.spot.concurrency
+
+import dev.zacsweers.metro.Qualifier
+
+@Qualifier public annotation class ApplicationScope
